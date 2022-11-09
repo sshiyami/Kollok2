c6cb1d7f6ab30110f23f01f0b7159b184782d07f
burkkys@gmail.com <burkkys@gmail.com>
2022-11-10 01:09:48 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        calculator calc = new calculator();
+        System.out.println(calc.Sum(3, 4));
+        System.out.println(calc.Sub(3, 4));
+        System.out.println(calc.Div(3, 4));
+        System.out.println(calc.Proizv(3, 4));
+    }
+}
